c90fdcbd33d131c3ac3b02b4a3194ed414fe09dc
Dua MG <duamahargul@outlook.com>
2022-07-18 09:56:41 +0500

    Initial commit

---
+++ src/PostList.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+function PostList() {
+    return (
+        <div>
+            Post List
+            <br/>
+            <span>Title: {todo.title}</span>
+        </div>
+            );
+}
+
+export default PostList;
